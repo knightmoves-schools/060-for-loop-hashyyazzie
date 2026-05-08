142d785958971cf907c49b42377cf3e63e0cabb5
Craig <craigyazzie143@icloud.com>
2026-05-08 04:26:48 -0700

    Update index.js
    gg

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+/**
+ * Prepends 'done - ' to each todo description in the array.
+ * @param {string[]} todos - An array of todo descriptions.
+ * @returns {string[]} - The modified array of todos.
+ */
 function markAsDone(todos) {
-  todos.forEach(todo => {
-    todo.description = `done - ${todo.description}`;
-  });
-
-  return todos;
+  return todos.map(todo => `done - ${todo}`);
 }
+
+// Example usage:
+const myTodos = ['buy milk', 'clean room', 'call mom'];
+const finishedTodos = markAsDone(myTodos);
+
+console.log(finishedTodos); 
+// Output: ['done - buy milk', 'done - clean room', 'done - call mom']
