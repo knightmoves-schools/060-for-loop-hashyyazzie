fa29791456ddc413fd10ebf81f791e020e4f92ef
Craig <craigyazzie143@icloud.com>
2026-05-08 04:20:38 -0700

    Update index.js
    gg

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function markAsDone(todos) {
-  for (let i = 0; i < todos.length; i++) {
-    todos[i].description = "done - " + todos[i].description;
-  }
+  todos.forEach(function(todo) {
+    todo.description = "done - " + todo.description;
+  });
 
   return todos;
 }
